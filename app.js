a8f6387ed34b25bcc1b82fe41d13eb86fc7bf874
Abhinav-FP <a.mathur@internetbusinesssolutionsindia.com>
2025-07-11 18:10:52 +0530

    Added db file

---
+++ app.js
@@ -0,0 +1,32 @@
+const dotenv = require("dotenv");
+dotenv.config();
+require("./dbconfigration");
+const express = require("express");
+const app = express();
+const cors = require("cors");
+
+const corsOptions = {
+  origin: "*", // Allowed origins
+  methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+  allowedHeaders: '*', // Allow all headers
+  credentials: true,
+  optionsSuccessStatus: 200, // for legacy browsers
+}
+app.use(cors(corsOptions));
+
+app.use(express.json({ limit: '2000mb' }));
+app.use(express.urlencoded({ extended: true, limit: "2000mb" }));
+
+const PORT = process.env.REACT_APP_SERVER_DOMAIN || 5000;
+
+// app.use("/api", require("./route/userRoutes"));
+
+app.get("/", (req, res) => {
+  res.json({
+    msg: 'Hello World',
+    status: 200,
+  });
+});
+
+const server = app.listen(PORT, () => console.log("Server is running at port : " + PORT));
+server.timeout = 360000;
