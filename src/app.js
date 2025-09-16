60f88ae414f95af50d9ded04c28ae8280f086f4d
Abhinav-FP <a.mathur@internetbusinesssolutionsindia.com>
2025-09-16 11:27:46 +0530

    Changed structure for vercel deploy 5

--- src/app.js
+++ src/app.js
@@ -22,12 +22,12 @@ app.use(express.urlencoded({ extended: true, limit: "2000mb" }));
 
 const PORT = process.env.REACT_APP_SERVER_DOMAIN || 5000;
 
-// app.use("/api", require("./route/userRoutes"));
-// app.use("/", require("./route/rssRoutes"));
-// app.use("/api", require("./route/fileRoutes"));
-// app.use("/api", require("./route/subscriberRoutes"));
-// app.use("/api", require("./route/contactRoutes"));
-// app.use("/api", require("./route/adminRoutes"));
+app.use("/api", require("./route/userRoutes"));
+app.use("/", require("./route/rssRoutes"));
+app.use("/api", require("./route/fileRoutes"));
+app.use("/api", require("./route/subscriberRoutes"));
+app.use("/api", require("./route/contactRoutes"));
+app.use("/api", require("./route/adminRoutes"));
 
 
 app.get("/", (req, res) => {
@@ -36,10 +36,6 @@ app.get("/", (req, res) => {
     status: 200,
   });
 });
-// const server = app.listen(PORT, () =>
-//   console.log("Server is running at port : " + PORT)
-// );
-// // server.timeout = 360000;
-// module.exports = serverless(app);
+
 const server = app.listen(PORT, () => console.log("Server is running at port : " + PORT));
 server.timeout = 360000;
