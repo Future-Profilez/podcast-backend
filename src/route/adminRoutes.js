58856397305a2d6bcda1248ca5243ce9949677c3
Abhinav-FP <a.mathur@internetbusinesssolutionsindia.com>
2025-09-16 12:13:16 +0530

    adding routes 6

--- src/route/adminRoutes.js
+++ src/route/adminRoutes.js
@@ -1,10 +1,10 @@
 const { AddPodcast, GetAllPodcasts, PodcastsDetail, GetAllPodcastswithFiles, UpdatePodcast, AddEpisode, GetEpisodeByUUID, GetAllEpisodes, UpdateEpisode, DeleteEpisode, DisablePodcast, AddGuide, UpdateGuide, GetAllGuides } = require("../controller/adminController");
 const router = require("express").Router();
 const { verifyToken } = require("../utils/tokenVerify");
-const { upload } = require("../utils/FileUploader");
+// const { upload } = require("../utils/FileUploader");
 
 // router.post("/admin/podcast/add", verifyToken, upload.single('thumbnail'), AddPodcast);
-router.get("/admin/podcast/get", GetAllPodcasts);
+// router.get("/admin/podcast/get", GetAllPodcasts);
 // router.get("/admin/podcast/get-detail/all", GetAllPodcastswithFiles);
 // router.get("/admin/podcast/get/:id", PodcastsDetail);
 // router.post("/admin/podcast/update/:id", verifyToken,  upload.single('thumbnail'), UpdatePodcast);
