a4d3cad7a6c7adc5ca29a19c6b4f7ddba4156e9a
Abhinav-FP <a.mathur@internetbusinesssolutionsindia.com>
2025-09-16 12:11:59 +0530

    adding routes 6

--- src/route/adminRoutes.js
+++ src/route/adminRoutes.js
@@ -5,24 +5,24 @@ const { upload } = require("../utils/FileUploader");
 
 // router.post("/admin/podcast/add", verifyToken, upload.single('thumbnail'), AddPodcast);
 router.get("/admin/podcast/get", GetAllPodcasts);
-router.get("/admin/podcast/get-detail/all", GetAllPodcastswithFiles);
-router.get("/admin/podcast/get/:id", PodcastsDetail);
+// router.get("/admin/podcast/get-detail/all", GetAllPodcastswithFiles);
+// router.get("/admin/podcast/get/:id", PodcastsDetail);
 // router.post("/admin/podcast/update/:id", verifyToken,  upload.single('thumbnail'), UpdatePodcast);
-router.delete("/admin/podcast/delete/:id", verifyToken, DisablePodcast);
+// router.delete("/admin/podcast/delete/:id", verifyToken, DisablePodcast);
 
 // router.post("/admin/file/add", verifyToken, upload.fields([
 //     { name: 'video', maxCount: 1 },
 //     { name: 'thumbnail', maxCount: 1 },
 //     ]), AddEpisode);
      
-router.get("/admin/file/getAll", GetAllEpisodes);
-router.get("/admin/file/get/:id", GetEpisodeByUUID);
+// router.get("/admin/file/getAll", GetAllEpisodes);
+// router.get("/admin/file/get/:id", GetEpisodeByUUID);
     
 // router.post("/admin/file/update/:id", verifyToken, upload.fields([
 //     { name: 'video', maxCount: 1 },
 //     { name: 'thumbnail', maxCount: 1 },
 //     ]), UpdateEpisode);
-router.delete("/admin/file/delete/:id", verifyToken, DeleteEpisode);
+// router.delete("/admin/file/delete/:id", verifyToken, DeleteEpisode);
 
 // router.post("/admin/guide/add", verifyToken, upload.fields([
 //     { name: 'guide', maxCount: 1 },
@@ -32,6 +32,6 @@ router.delete("/admin/file/delete/:id", verifyToken, DeleteEpisode);
 //     { name: 'guide', maxCount: 1 },
 //     { name: 'thumbnail', maxCount: 1 },
 //     ]), UpdateGuide);
-router.get("/admin/guide/get", GetAllGuides);
+// router.get("/admin/guide/get", GetAllGuides);
 
 module.exports = router;
