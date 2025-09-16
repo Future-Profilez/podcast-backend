0a110110e5637d3082130eef8ba9826cdbf0a9d9
Abhinav-FP <a.mathur@internetbusinesssolutionsindia.com>
2025-09-16 12:10:44 +0530

    adding routes 5

--- src/route/adminRoutes.js
+++ src/route/adminRoutes.js
@@ -3,35 +3,35 @@ const router = require("express").Router();
 const { verifyToken } = require("../utils/tokenVerify");
 const { upload } = require("../utils/FileUploader");
 
-router.post("/admin/podcast/add", verifyToken, upload.single('thumbnail'), AddPodcast);
+// router.post("/admin/podcast/add", verifyToken, upload.single('thumbnail'), AddPodcast);
 router.get("/admin/podcast/get", GetAllPodcasts);
 router.get("/admin/podcast/get-detail/all", GetAllPodcastswithFiles);
 router.get("/admin/podcast/get/:id", PodcastsDetail);
-router.post("/admin/podcast/update/:id", verifyToken,  upload.single('thumbnail'), UpdatePodcast);
+// router.post("/admin/podcast/update/:id", verifyToken,  upload.single('thumbnail'), UpdatePodcast);
 router.delete("/admin/podcast/delete/:id", verifyToken, DisablePodcast);
 
-router.post("/admin/file/add", verifyToken, upload.fields([
-    { name: 'video', maxCount: 1 },
-    { name: 'thumbnail', maxCount: 1 },
-    ]), AddEpisode);
+// router.post("/admin/file/add", verifyToken, upload.fields([
+//     { name: 'video', maxCount: 1 },
+//     { name: 'thumbnail', maxCount: 1 },
+//     ]), AddEpisode);
      
 router.get("/admin/file/getAll", GetAllEpisodes);
 router.get("/admin/file/get/:id", GetEpisodeByUUID);
     
-router.post("/admin/file/update/:id", verifyToken, upload.fields([
-    { name: 'video', maxCount: 1 },
-    { name: 'thumbnail', maxCount: 1 },
-    ]), UpdateEpisode);
+// router.post("/admin/file/update/:id", verifyToken, upload.fields([
+//     { name: 'video', maxCount: 1 },
+//     { name: 'thumbnail', maxCount: 1 },
+//     ]), UpdateEpisode);
 router.delete("/admin/file/delete/:id", verifyToken, DeleteEpisode);
 
-router.post("/admin/guide/add", verifyToken, upload.fields([
-    { name: 'guide', maxCount: 1 },
-    { name: 'thumbnail', maxCount: 1 },
-    ]), AddGuide);
-router.post("/admin/file/update/:id", verifyToken, upload.fields([
-    { name: 'guide', maxCount: 1 },
-    { name: 'thumbnail', maxCount: 1 },
-    ]), UpdateGuide);
+// router.post("/admin/guide/add", verifyToken, upload.fields([
+//     { name: 'guide', maxCount: 1 },
+//     { name: 'thumbnail', maxCount: 1 },
+//     ]), AddGuide);
+// router.post("/admin/file/update/:id", verifyToken, upload.fields([
+//     { name: 'guide', maxCount: 1 },
+//     { name: 'thumbnail', maxCount: 1 },
+//     ]), UpdateGuide);
 router.get("/admin/guide/get", GetAllGuides);
 
 module.exports = router;
